2ec14afad5558f7d56afae619a1abe015941da60
L1nexi <l1nexi@outlook.com>
2023-10-13 11:54:58 +0800

    Adding arguments

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*- 
 import re
 import os
+import argparse
 
 # TODO 1: 增加启动参数以决定处理的元件类型
 # TODO 2: 支持数组元件的处理
@@ -85,22 +86,53 @@ def handleArrLike(name, fin, fout):
             str = input()
             continue
     fout.writelines(tmpList)
-    
 
-try:
-    with open("E:\\github\\BASYS3-constrains-modifier\\Basys3_Master.xdc", "r") as fin, open("E:\\github\\BASYS3-constrains-modifier\\Basys3_Master_new.xdc", "w", encoding="utf-8") as fout:
-        for line in fin:
-            if "Switches" in line:
-                fout.write(line)
-                handleArrLike("sw", fin, fout)
-            elif "LEDs" in line:
-                fout.write(line)
-                handleArrLike("led", fin, fout)
-            else:
-                fout.write(line)
-                
+def handleArg():
+    parser = argparse.ArgumentParser(description="Modify constrains file for BASYS3")
+    parser.add_argument("-i", "--input", default="Basys3_Master.xdc", help="Input file name. Stored as text.")
+    parser.add_argument("-o", "--output", default="Basys3_Master_new.xdc", help="Output file name.")
+    parser.add_argument("-L","--LED", help="Add LEDs to the handling lists.", action="store_true")
+    parser.add_argument("-sw", "--switch", help="Add switches to the handling lists.", action="store_true")
+    arg = parser.parse_args()
+    return arg
 
-except KeyboardInterrupt:
-    print("KeyboardInterrupt")
-    exit(0)
-    
+def generateList(arg):
+    procFlag = False
+    procFlag = procFlag | arg.LED | arg.switch
+    procFlag = not procFlag;
+    list = []
+    if (procFlag == True):
+        list = ["sw", "led"]
+    else:
+        if arg.switch:
+            list.append("sw")
+        if arg.LED:
+            list.append("led")
+    return list
+
+def main():
+    global _NAME_
+    arg = handleArg()
+    list = generateList(arg)
+
+    try:
+        j = 0
+        with open(arg.input, "r", encoding="utf-8") as fin, open(arg.output, "w", encoding="utf-8") as fout:
+            curTarget = _NAME_[list[j]]
+            for line in fin:
+                if j < list.__len__() and curTarget in line:
+                    fout.write(line)
+                    handleArrLike(list[j], fin, fout)
+                    j += 1
+                    if j < list.__len__():
+                        curTarget = _NAME_[list[j]]
+
+                else:
+                    fout.write(line)
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+        exit(0)
+
+if __name__ == "__main__":
+    main()
+        
