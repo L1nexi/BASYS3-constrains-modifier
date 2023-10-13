46e4dda232f724c4a14d2d7905c7fd411dc10cb3
L1nexi <l1nexi@outlook.com>
2023-10-13 10:18:09 +0800

    增加数组形式的处理

--- main.py
+++ main.py
@@ -4,33 +4,91 @@ import re
 import os
 
 # TODO 1: 增加启动参数以决定处理的元件类型
+# TODO 2: 支持数组元件的处理
 
 _NUMS_ = {"led": 16, "sw": 16, "seg": 7, "an": 4}
-def handleArrLike(name, fin, fout):
-    tmpList = []
+_NAME_ = {"led": "LEDs", "sw": "Switches", "seg": "SEG", "an": "AN"}
+
+def checkIndex(index, name):
     global _NUMS_
+    if int(index) < 0 or int(index) >= _NUMS_[name]:
+        return False
+    return True
+
+def handleArrLike(name, fin, fout):
+    global _NUMS_
+    tmpList = []
+
     for i in range(_NUMS_[name] * 2):
         tmpList.append(fin.readline())
-    print("Please input the index of {0} (numbered from 0) and its new tag. Input \'Done\' when finished.".format(name))
+
+    print("Start processing {}...".format(_NAME_[name]))
+    print("Input the index of {} and the new tag. Input \'Done\' to finish.".format(_NAME_[name]))
     str = input();
     while (str != "Done"):
-        index, tag = str.split()
-        if int(index) < 0 or int(index) >= _NUMS_[name]:
-            print("Invalid index. Please input again or quit by \'Done\'?")
+        try:
+            if str.split().__len__() == 3:
+                begin, end, tag = str.split()
+                begin = int(begin)
+                end = int(end)
+            elif str.split().__len__() == 2:
+                begin, tag = str.split()
+                end = begin = int(begin)
+            else:
+                print("Invalid input. Please input again or input \'Done\' to finish.")
+                str = input()
+                continue
+
+
+            if checkIndex(begin, name) == False or checkIndex(end, name) == False:
+                print("Index out of bounds. Please input again or input \'Done\' to finish.?")
+                str = input()
+                continue
+
+            # begin == end : 单个处理
+            if begin == end:
+                pattern = name + "[{}]".format(begin)
+                tmpList[2 * begin] = re.sub(re.escape(pattern), tag, tmpList[2 * begin])
+                tmpList[2 * begin] = re.sub("#", "", tmpList[2 * begin])
+                tmpList[2 * begin + 1] = re.sub(re.escape(pattern), tag, tmpList[2 * begin + 1])
+                tmpList[2 * begin + 1] = re.sub("#", "", tmpList[2 * begin + 1])
+            
+            # begin <= end : 按数组处理
+
+            elif int(begin) < int(end):
+                j = 0
+                for i in range(begin, end + 1):
+                    pattern = name + "[{}]".format(i)
+                    arrTag = tag + "[{}]".format(j)
+                    tmpList[2 * i] = re.sub(re.escape(pattern), arrTag, tmpList[2 * i])
+                    tmpList[2 * i] = re.sub("#", "", tmpList[2 * i])
+                    tmpList[2 * i + 1] = re.sub(re.escape(pattern), arrTag, tmpList[2 * i + 1])
+                    tmpList[2 * i + 1] = re.sub("#", "", tmpList[2 * i + 1])
+                    j += 1
+
+            else :
+                j = 0;
+                for i in range(begin, end - 1, -1):
+                    pattern = name + "[{}]".format(i)
+                    arrTag = tag + "[{}]".format(j)
+                    tmpList[2 * i] = re.sub(re.escape(pattern), arrTag, tmpList[2 * i])
+                    tmpList[2 * i] = re.sub("#", "", tmpList[2 * i])
+                    tmpList[2 * i + 1] = re.sub(re.escape(pattern), arrTag, tmpList[2 * i + 1])
+                    tmpList[2 * i + 1] = re.sub("#", "", tmpList[2 * i + 1])
+                    j += 1
+
+            print("Handled.\nNext one or quit by \'Done\'?")
+            str = input()
+
+        except ValueError:
+            print("Invalid input. Please input again or input \'Done\' to finish.")
             str = input()
             continue
-        targetStr = name + "[{}]".format(index)
-        tmpList[2 * int(index)] = re.sub(re.escape(targetStr), tag, tmpList[2 * int(index)])
-        tmpList[2 * int(index)] = re.sub("#", "", tmpList[2 * int(index)])
-        tmpList[2 * int(index) + 1] = re.sub(re.escape(targetStr), tag, tmpList[2 * int(index) + 1])
-        tmpList[2 * int(index) + 1] = re.sub("#", "", tmpList[2 * int(index) + 1])
-        print("Handled.\n Next one or quit by \'Done\'?")
-        str = input()
     fout.writelines(tmpList)
     
 
 try:
-    with open("Basys3_Master.xdc", "r") as fin, open("Basys3_Master_new.xdc", "w", encoding="utf-8") as fout:
+    with open("E:\\github\\BASYS3-constrains-modifier\\Basys3_Master.xdc", "r") as fin, open("E:\\github\\BASYS3-constrains-modifier\\Basys3_Master_new.xdc", "w", encoding="utf-8") as fout:
         for line in fin:
             if "Switches" in line:
                 fout.write(line)
@@ -41,6 +99,7 @@ try:
             else:
                 fout.write(line)
                 
+
 except KeyboardInterrupt:
     print("KeyboardInterrupt")
     exit(0)
